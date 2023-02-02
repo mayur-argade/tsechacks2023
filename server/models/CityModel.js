a3d8feb1d870e9ff2c81a50f26a6590577ac0b03
Mayur Argade <argademayur2002@gmail.com>
2023-02-02 12:57:02 +0530

    city and flat model updated

---
+++ server/models/CityModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const citySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please provide a name"],
+      maxLength: [40, "name should be under 40 characters"],
+    },
+    state: {
+      type: String,
+    },
+    crimeRate: {
+      type: String,
+    },
+    COL: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("City", citySchema);
